eee9e71902bdc406cb4879b7a329a73f6cadee00
hafsa_mohamed <hafsa.mohammed@t3.com.eg>
2025-10-01 22:11:27 +0300

    edited the whole layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import WhyChoose from "./components/choose";
 import FeaturedEquipment from "./components/FeaturedEquipment";
 import Footer from "./components/Footer";
-import Header from "./components/header"
+import Header from "./components/theheader"
 
 
 export default function Home() {
