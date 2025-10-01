cc0547603935ceb6249c2ba76783b55b2cf6dac2
hafsa_mohamed <hafsa.mohammed@t3.com.eg>
2025-10-01 22:33:06 +0300

    edited eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,18 +19,12 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
-  },
-
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      react,
-    },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
     },
   },
+
 ];
 
 export default eslintConfig;
